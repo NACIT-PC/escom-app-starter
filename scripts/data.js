0eaa11d18f62630e589d0afcef96ea6cb56d7dcd
Daniel P. Mgawi <pdmgawi@gmail.com>
2024-11-11 16:43:28 +0200

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -58,7 +58,8 @@ function populateTableData() {
     for (const location of regionData.locations) {
         console.log(location);
         const row = document.createElement("tr");
-        row.innerHTML = `<td>${location.id}</td><td>${location.name}</td><td>${location.locations}</td><td>${location.locations}</td>`;
+        row.innerHTML = `<td>${location.id}</td><td>${location.name}</td><td>${location.locations}</td>`;
+        // row.innerHTML = `<td>${location.id}</td><td>${location.name}</td><td>${location.locations}</td><td>${location.locations}</td>`;
         locationSelect.appendChild(row);
     }
 }
