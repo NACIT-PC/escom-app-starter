f9c0eb22ed70c844ceccd8c04ac3610040c19554
Kennie265 <kennedyphiri2018@gmail.com>
2024-11-18 13:14:18 +0200

    changes

--- scripts/api.js
+++ scripts/api.js
@@ -31,11 +31,7 @@ function populateAreaDropdown() {
 
   console.log("Area Element", areaSelect);
 
-  // if (regionData.areas.length > 1) {
-  //     areaSelect.remove(0);
-  //     const option = new Option("Select Area", "#");
-  //     areaSelect.add(option);
-  // }
+ 
 
   // Clear existing options except the first one (Select Region)
   if (areaSelect.options.length > 1) {
